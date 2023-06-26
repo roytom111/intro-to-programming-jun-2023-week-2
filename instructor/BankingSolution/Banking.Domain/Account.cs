0ab68abd4bf6e66a4c33e4e28cbed32b5bcc09ef
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-26 10:50:16 -0400

    Initialize gold-customer-flags

---
+++ instructor/BankingSolution/Banking.Domain/Account.cs
@@ -0,0 +1,35 @@
+﻿namespace Banking.Domain;
+
+public enum LoyaltyLevel {  Standard, Gold }
+public class Account
+{
+    private decimal _balance = 5000; // Fields class level variable
+    public LoyaltyLevel AccountType { get; set; } = LoyaltyLevel.Standard;
+    public void Deposit(decimal amountToDeposit)
+    {
+       
+        if(AccountType == LoyaltyLevel.Gold)
+        {
+            _balance += amountToDeposit * 1.10M;
+        } else
+        {
+            _balance += amountToDeposit;
+        }
+        
+    }
+
+    
+    public decimal GetBalance()
+    {
+        return _balance; // "Sliming"
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        if(amountToWithdraw > _balance)
+        {
+            throw new OverdraftException();
+        }
+       _balance -= amountToWithdraw;
+    }
+}
---
+++ instructor/BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+namespace Banking.UnitTests.BankAccount;
+public class GoldCustomersGetABonusOnDeposits
+{
+    [Fact]
+    public void BonusIsApplied()
+    {
+        // Given - the "Context" for this test.
+        var account = new Account();
+        account.AccountType = LoyaltyLevel.Gold;
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+        var expectedNewBalance = openingBalance + amountToDeposit + 10M;
+
+
+        // When - what we are testing
+        account.Deposit(amountToDeposit);
+
+        // Then - how do we know it worked?
+        Assert.Equal(expectedNewBalance, account.GetBalance());
+
+    }
+}
