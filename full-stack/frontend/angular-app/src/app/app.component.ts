f63143a28b138af0ebadeca335adef8082354206
roytom111 <rf376519@ohio.edu>
2023-06-26 14:56:59 -0400

    have app created

---
+++ full-stack/frontend/angular-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { BankAccountComponent } from './components/bank-account.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, BankAccountComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = "Jeff"
+}
+
---
+++ full-stack/frontend/angular-app/src/app/components/bank-account.component.ts
@@ -0,0 +1,50 @@
+import { CurrencyPipe } from "@angular/common";
+import { Component, signal } from "@angular/core";
+
+
+
+@Component({
+    selector: 'app-account',
+    template: `
+        <section>
+<span>Your balance is {{balance() | currency}}</span>
+</section>
+<section>
+<label>Amount: <input type="number" #amount /></label>
+</section>
+<section>
+<button (click)="deposit(amount)">Deposit</button>
+<button (click)="withdraw(amount)">Withdraw</button>
+</section>
+    `,
+    imports: [CurrencyPipe],
+    standalone: true
+})
+export class BankAccountComponent {
+
+
+
+    balance = signal(5000);
+
+
+    constructor() {
+
+    }
+
+
+
+
+    deposit(amount: HTMLInputElement) {
+        this.balance.set(this.balance() + amount.valueAsNumber);
+        amount.value = '';
+        amount.focus();
+    }
+
+
+
+    withdraw(amount: HTMLInputElement) {
+        this.balance.set(this.balance() - amount.valueAsNumber);
+        amount.value = '';
+        amount.focus();
+    }
+}
