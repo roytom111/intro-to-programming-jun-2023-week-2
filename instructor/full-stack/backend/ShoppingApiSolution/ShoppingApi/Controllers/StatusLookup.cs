eebf00632c81f4ce75c408f74609abddd823c10b
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-28 11:06:25 -0400

    dev environment

---
+++ instructor/full-stack/backend/ShoppingApiSolution/ShoppingApi/Controllers/StatusLookup.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingApi.Controllers;
+
+public class StatusLookup : ILookupTheStatus
+{
+    public Task<GetStatusResponse> GetCurrentStatusAsync()
+    {
+
+        var response = new GetStatusResponse
+        {
+            Message = "Looks Good Here",
+            LastChecked = DateTime.Now.AddMinutes(-9)
+        };
+        return Task.FromResult(response);
+    }
+}
